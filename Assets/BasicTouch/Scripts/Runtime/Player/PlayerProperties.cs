a2b0b22534ba6a28b44f31228c897ec4b5918c39
ONUR\onuri <onurinaldev@gmail.com>
2026-02-04 02:26:52 +0300

    refactor PlayerProperties

--- Assets/BasicTouch/Scripts/Runtime/Player/PlayerProperties.cs
+++ Assets/BasicTouch/Scripts/Runtime/Player/PlayerProperties.cs
@@ -2,11 +2,17 @@
 
 namespace BasicTouch.Runtime.Player
 {
+    /// <summary>
+    /// Manages player data
+    /// </summary>
     [CreateAssetMenu(fileName = "PlayerProperties", menuName = "BasicTouch/PlayerProperties")]
     public class PlayerProperties : ScriptableObject
     {
         [SerializeField] private float m_MoveSpeed = 8;
 
+        /// <summary>
+        /// Player movement speed
+        /// </summary>
         public float MoveSpeed => m_MoveSpeed;
     }
 }
