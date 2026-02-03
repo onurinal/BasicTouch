8fb2e7ce3ace1b8b91dec1774d22c4e2a757368a
ONUR\onuri <onurinaldev@gmail.com>
2026-02-04 02:59:41 +0300

    refactor IInteractable

--- Assets/BasicTouch/Scripts/Runtime/Core/IInteractable.cs
+++ Assets/BasicTouch/Scripts/Runtime/Core/IInteractable.cs
@@ -2,10 +2,30 @@
 
 namespace BasicTouch.Runtime.Core
 {
+    /// <summary>
+    /// Defines a base contract for all interactable objects.
+    /// </summary>
     public interface IInteractable
     {
+        #region Public Methods
+
+        /// <summary>
+        /// Executes the interaction logic.
+        /// </summary>
         void Interact();
+
+        /// <summary>
+        /// Gets the interaction text displayed to the player.
+        /// </summary>
+        /// <returns>Interaction prompt text.</returns>
         string GetInteractText();
+
+        /// <summary>
+        /// Gets the transform of the interactable object.
+        /// </summary>
+        /// <returns>The transform of the interactable.</returns>
         Transform GetTransform();
+
+        #endregion
     }
 }
