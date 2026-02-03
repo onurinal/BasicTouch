e277dc912ec8844b8bbc7790cef6fbf6b22883ac
ONUR\onuri <onurinaldev@gmail.com>
2026-02-03 19:35:03 +0300

    refactor ( PlayerController ) : player can move now with camera direction

--- Assets/BasicTouch/Scripts/Runtime/Player/PlayerController.cs
+++ Assets/BasicTouch/Scripts/Runtime/Player/PlayerController.cs
@@ -4,28 +4,31 @@ namespace BasicTouch.Runtime.Player
 {
     public class PlayerController : MonoBehaviour
     {
+        [Header("References")]
+        [SerializeField] private PlayerProperties m_PlayerProperties;
         [SerializeField] private PlayerInputHandler m_playerInputHandler;
         [SerializeField] private Rigidbody m_rigidbody;
-        [SerializeField] private PlayerProperties m_playerProperties;
+
+        private Vector3 m_movementDirection;
+        private Camera m_mainCamera;
 
         private void Awake()
         {
-            if (m_playerInputHandler == null)
-            {
-                Debug.LogWarning("PlayerInputHandler is null");
-                return;
-            }
-
-            if (m_rigidbody == null)
-            {
-                Debug.LogWarning("Rigidbody is null");
-                return;
-            }
-
-            if (m_playerProperties == null)
+            if (m_PlayerProperties == null)
             {
                 Debug.LogWarning("PlayerProperties is null");
+                return;
             }
+
+            if (m_mainCamera == null)
+            {
+                m_mainCamera = Camera.main;
+            }
+        }
+
+        private void Update()
+        {
+            CalculateMoveDirection();
         }
 
         private void FixedUpdate()
@@ -33,12 +36,26 @@ namespace BasicTouch.Runtime.Player
             Move();
         }
 
+        private void CalculateMoveDirection()
+        {
+            Vector2 inputDirection = m_playerInputHandler.MoveInput;
+
+            Vector3 cameraForward = m_mainCamera.transform.forward;
+            Vector3 cameraRight = m_mainCamera.transform.right;
+
+            cameraForward.y = 0;
+            cameraRight.y = 0;
+
+            m_movementDirection = (inputDirection.y * cameraForward + inputDirection.x * cameraRight).normalized;
+        }
+
         private void Move()
         {
-            Vector2 direction = m_playerInputHandler.MoveInput;
-            direction.Normalize();
-            Vector2 movement = direction * m_playerProperties.MoveSpeed;
-            m_rigidbody.linearVelocity = new Vector3(movement.x, m_rigidbody.linearVelocity.y, movement.y);
+            if (m_movementDirection != Vector3.zero)
+            {
+                Vector3 movement = m_movementDirection * m_PlayerProperties.MoveSpeed;
+                m_rigidbody.linearVelocity = new Vector3(movement.x, m_rigidbody.linearVelocity.y, movement.z);
+            }
         }
     }
 }
