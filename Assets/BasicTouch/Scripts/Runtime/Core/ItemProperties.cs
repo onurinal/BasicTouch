3167198905286848377b83bdf922b1ce65489871
ONUR\onuri <onurinaldev@gmail.com>
2026-02-04 02:59:54 +0300

    refactor ItemProperties

--- Assets/BasicTouch/Scripts/Runtime/Core/ItemProperties.cs
+++ Assets/BasicTouch/Scripts/Runtime/Core/ItemProperties.cs
@@ -2,14 +2,31 @@
 
 namespace BasicTouch.Runtime.Core
 {
+    /// <summary>
+    /// Contains items data
+    /// </summary>
     [CreateAssetMenu(fileName = "Item", menuName = "BasicTouch/Item")]
     public class ItemProperties : ScriptableObject
     {
+        #region Fields
+
         [SerializeField] private string m_ItemName;
         [SerializeField] private int m_ItemID;
 
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Item'ın görünen adı.
+        /// </summary>
         public string ItemName => m_ItemName;
 
+        /// <summary>
+        /// Item'ın ID'si.
+        /// </summary>
         public int ItemID => m_ItemID;
+
+        #endregion
     }
 }
